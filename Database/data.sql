c4678774f2b76e7629609aae0aad8b6a325db288
Huỳnh Long Hải <shark050200@gmail.com>
2020-12-26 11:17:14 +0700

    add database

---
+++ Database/data.sql
@@ -0,0 +1,15 @@
+insert into producers(producername, producerdescription, producerimage) values ('Vinfast', '', 'vinfast-logo.png');
+insert into producers(producername, producerdescription, producerimage) values ('HONDA', '', 'honda-logo.png');
+insert into producers(producername, producerdescription, producerimage) values ('Mercedes Benz', '', 'mercedesbenz-logo.png');
+insert into producers(producername, producerdescription, producerimage) values ('TOYOTA', '', 'toyota-logo.png');
+insert into producers(producername, producerdescription, producerimage) values ('ford', '', 'ford-logo.png');
+insert into producers(producername, producerdescription, producerimage) values ('BMW', '', 'bmw-logo.png');
+
+insert into origins(originname, origindescription) values ('Vietnam', 'Made in Vietnam');
+insert into origins(originname, origindescription) values ('America', 'Made in America');
+insert into origins(originname, origindescription) values ('China', 'Made in China');
+insert into origins(originname, origindescription) values ('Germany', 'Made in Germany');
+
+insert into categories(categoryname, categorydescription, categoryimage) values ('Electric Motorcycle', '', '');
+insert into categories(categoryname, categorydescription, categoryimage) values ('Motorcycle', '', '');
+insert into categories(categoryname, categorydescription, categoryimage) values ('Car', '', '');
