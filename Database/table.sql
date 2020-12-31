bab5e3c428789d693260d6b49db4edca9661ff09
Huỳnh Long Hải <shark050200@gmail.com>
2020-12-31 11:45:03 +0700

    Updated full table

--- Database/table.sql
+++ Database/table.sql
@@ -35,3 +35,48 @@ create table Products (
     foreign key (OriginId) references origins(OriginId),
     foreign key (ProducerId) references Producers(ProducerId)
 );
+
+create table Customers (
+	CustomerId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	CustomerFullName NVARCHAR(200) NOT NULL,
+	CustomerAddress NVARCHAR(1000) NOT NULL,
+	CustomerTel NVARCHAR(200),
+    CustomerUsername NVARCHAR(200),
+    CustomerPassword NVARCHAR(200),
+    UNIQUE (CustomerUsername)
+);
+
+create table Employees (
+	EmployeesId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	EmployeesFullName NVARCHAR(200) NOT NULL,
+	EmployeesAddress NVARCHAR(1000) NOT NULL,
+	EmployeesTel NVARCHAR(200),
+    EmployeesUsername NVARCHAR(200),
+    EmployeesPassword NVARCHAR(200),
+    UNIQUE (EmployeesUsername)
+);
+create table OrderState (
+	StateId int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Description NVARCHAR(200) NOT NULL
+);
+
+create table Orders (
+	OrderId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	CustomerId int unsigned,
+	StateId int UNSIGNED,
+	CreateAt DateTime default current_timestamp,
+    TotalPrice int,
+    foreign key (CustomerId) references Customers(CustomerId),
+    foreign key (State) references OrderState(StateKey)
+);
+
+create table OrderDetails (
+	OrderId INT UNSIGNED,
+	ProductId int unsigned,
+    Quantity int not null,
+    UnitPrice int not null,
+    SubTotalPrice int as (Quantity * UnitPrice),
+    foreign key (OrderId) references Orders(OrderId),
+    foreign key (ProductId) references Products(ProductId),
+    primary key (OrderId, ProductId)
+);
